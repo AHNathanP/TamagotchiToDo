f174909458aaab81eddb6507a72a02a3d3303e10
AHNathanP <philipsn24@hsnet.ahsd.org>
2023-04-19 13:01:18 -0400

    Wed, 4/19 school

--- app/src/main/java/com/example/tamagotchitodo/ToDoListFragment.kt
+++ app/src/main/java/com/example/tamagotchitodo/ToDoListFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.example.tamagotchitodo.databinding.FragmentToDoListBinding
 
 class ToDoListFragment : Fragment() {
@@ -16,8 +17,13 @@ class ToDoListFragment : Fragment() {
         _binding = FragmentToDoListBinding.inflate(inflater, container, false)
         val rootView = binding.root
 
-        val tasks = listOf(Task("Clean bedroom", "I need to clean my room", "02/03"),
+        val tasks = mutableListOf(Task("Clean bedroom", "I need to clean my room", "02/03"),
             Task("Buy groceries", "Be sure to get milk", "02/04"))
+
+        binding.addButton.setOnClickListener {
+            rootView.findNavController().navigate(R.id.action_toDoListFragment_to_addFragment)
+        }
+
         val myAdapter = TaskAdapter(tasks)
         binding.recyclerView.adapter = myAdapter
 
