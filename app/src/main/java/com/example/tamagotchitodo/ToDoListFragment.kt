f9211a8e93b5385618e5d4f00c06abd220531cd3
AHNathanP <philipsn24@hsnet.ahsd.org>
2023-04-22 21:46:15 -0400

    Sat, 4/22 firebase

--- app/src/main/java/com/example/tamagotchitodo/ToDoListFragment.kt
+++ app/src/main/java/com/example/tamagotchitodo/ToDoListFragment.kt
@@ -5,19 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.setFragmentResult
 import androidx.fragment.app.setFragmentResultListener
 import androidx.navigation.findNavController
 import com.example.tamagotchitodo.databinding.FragmentToDoListBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class ToDoListFragment : Fragment() {
     private var _binding: FragmentToDoListBinding? = null
     private val binding get() = _binding!!
+    lateinit var dbRef : DatabaseReference
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?): View? {
         _binding = FragmentToDoListBinding.inflate(inflater, container, false)
         val rootView = binding.root
+        dbRef = Firebase.database.reference
         val tasks = mutableListOf(Task("Clean bedroom", "02/03"), Task("Buy groceries", "02/04"))
 
         setFragmentResultListener("REQUESTING_NAME_KEY") { nameKey: String, bundle: Bundle ->
